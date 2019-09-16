f28fc4ea7da6c300ff072bc60221a0305812e832
Carmen <sdluojm@gmail.com>
2019-09-16 17:56:54 +0000

    create assingment and submission table

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS assignments, assignment_submissions;
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  duration INTEGER, 
+  submission_date DATE
+);
+
